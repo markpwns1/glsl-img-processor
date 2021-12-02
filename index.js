5afc3d0b3c576291efc99e05e8fab399f0630d6f
Mark  Kaldas <mark.kaldas@childrenslink.ca>
2021-12-02 14:16:51 -0700

    most functionality done

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ void main() {
 }
 `;
 
-let mainTex;
+// let mainTex;
 /** @type {WebGLRenderingContext} */
 let gl;
 let squareBuffer;
 let programInfo;
+
+let textures = { };
 
 //
 // Initialize a shader program, so WebGL knows how to draw our data
@@ -163,16 +165,20 @@ function drawScene(gl, programInfo, buffers) {
     // Tell WebGL to use our program when drawing
   
     gl.useProgram(programInfo.program);
-    // console.log(programInfo.code);
   
-    // Tell WebGL we want to affect texture unit 0
-    gl.activeTexture(gl.TEXTURE0);
+    const keys = Object.keys(textures);
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        
+        // Tell WebGL we want to affect texture unit 0
+        gl.activeTexture(gl.TEXTURE0 + i);
 
-    // Bind the texture to texture unit 0
-    gl.bindTexture(gl.TEXTURE_2D, mainTex);
+        // Bind the texture to texture unit 0
+        gl.bindTexture(gl.TEXTURE_2D, textures[key]);
 
-    // Tell the shader we bound the texture to texture unit 0
-    gl.uniform1i(programInfo.uniformLocations.maintex, 0);
+        // Tell the shader we bound the texture to texture unit 0
+        gl.uniform1i(gl.getUniformLocation(programInfo.program, key), i);
+    }
 
     {
         const offset = 0;
@@ -183,6 +189,9 @@ function drawScene(gl, programInfo, buffers) {
   
 
 function main() {
+
+    colourPicker();
+
     const canvas = document.querySelector("#glCanvas");
     // Initialize the GL context
     
@@ -199,20 +208,20 @@ function main() {
 
     squareBuffer = initBuffers(gl);
 
-    updateDimensions();
+    updateDimensions($("#output-dimensions").val(), true);
+    updateDimensions($("#view-dimensions").val(), false);
+    drawScene(gl, programInfo, squareBuffer);
 }
 
+// gl.getUniformLocation(shaderProgram, 'maintex')
 function reloadShader() {
-    const shaderProgram = initShaderProgram(gl, vsSource, $("#frag-shader-code").val());
+    const shaderProgram = initShaderProgram(gl, vsSource, editor.getValue());
     programInfo = {
         program: shaderProgram,
         // code: $("#frag-shader-code").text(),
         attribLocations: {
             vertexPosition: gl.getAttribLocation(shaderProgram, 'v_pos'),
             textureCoord: gl.getAttribLocation(shaderProgram, 'v_texcoord'),
-        },
-        uniformLocations: {
-            maintex: gl.getUniformLocation(shaderProgram, 'maintex'),
         }
     };
 }
@@ -221,7 +230,7 @@ function reloadShader() {
 // Initialize a texture and load an image.
 // When the image finished loading copy it into the texture.
 //
-function loadTexture(gl, url, onLoaded) {
+function loadTexture(gl, url, onLoaded, onFailed) {
     const texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
   
@@ -262,8 +271,11 @@ function loadTexture(gl, url, onLoaded) {
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
         }
 
-        onLoaded();
+        onLoaded(texture);
     };
+
+    image.onerror = onFailed;
+
     image.src = url;
   
     return texture;
@@ -273,64 +285,229 @@ function isPowerOf2(value) {
     return (value & (value - 1)) == 0;
 }
 
-function updateDimensions() {
-    const dimensions = $("#img-dimensions").val().split(",").map(x => x.trim());
+function colourPicker() {
+    const colorBlock = document.getElementById('color-block');
+    const ctx1 = colorBlock.getContext('2d');
+    const width1 = colorBlock.width;
+    const height1 = colorBlock.height;
+
+    const colorStrip = document.getElementById('color-strip');
+    const ctx2 = colorStrip.getContext('2d');
+    const width2 = colorStrip.width;
+    const height2 = colorStrip.height;
+
+    const colorLabel = document.getElementById('color-label');
+    const colorText = document.getElementById('color-text');
+
+    let x = width1;
+    let y = height1;
+    let drag = false;
+    let dragStrip = false;
+    let colourText = "255, 0, 0";
+    let rgbaColor = 'rgba(255,0,0,1)';
+
+    ctx1.rect(0, 0, width1, height1);
+    fillGradient();
+
+    ctx2.rect(0, 0, width2, height2);
+    var grd1 = ctx2.createLinearGradient(0, 0, 0, height1);
+    grd1.addColorStop(0, 'rgba(255, 0, 0, 1)');
+    grd1.addColorStop(0.17, 'rgba(255, 255, 0, 1)');
+    grd1.addColorStop(0.34, 'rgba(0, 255, 0, 1)');
+    grd1.addColorStop(0.51, 'rgba(0, 255, 255, 1)');
+    grd1.addColorStop(0.68, 'rgba(0, 0, 255, 1)');
+    grd1.addColorStop(0.85, 'rgba(255, 0, 255, 1)');
+    grd1.addColorStop(1, 'rgba(255, 0, 0, 1)');
+    ctx2.fillStyle = grd1;
+    ctx2.fill();
+
+    function fillGradient() {
+        ctx1.fillStyle = rgbaColor;
+        ctx1.fillRect(0, 0, width1, height1);
+
+        var grdWhite = ctx2.createLinearGradient(0, 0, width1, 0);
+        grdWhite.addColorStop(0, 'rgba(255,255,255,1)');
+        grdWhite.addColorStop(1, 'rgba(255,255,255,0)');
+        ctx1.fillStyle = grdWhite;
+        ctx1.fillRect(0, 0, width1, height1);
+
+        var grdBlack = ctx2.createLinearGradient(0, 0, 0, height1);
+        grdBlack.addColorStop(0, 'rgba(0,0,0,0)');
+        grdBlack.addColorStop(1, 'rgba(0,0,0,1)');
+        ctx1.fillStyle = grdBlack;
+        ctx1.fillRect(0, 0, width1, height1);
+    }
+
+    function changeColor(e) {
+        if(e) {
+            x = e.offsetX;
+            y = e.offsetY;
+        }
+        var imageData = ctx1.getImageData(x, y, 1, 1).data;
+        rgbaColor = 'rgba(' + imageData[0] + ',' + imageData[1] + ',' + imageData[2] + ',1)';
+        colorLabel.style.backgroundColor = rgbaColor;
+        let r = Math.round(imageData[0] / 255.0 * 1000.0) / 1000.0;
+        let g = Math.round(imageData[1] / 255.0 * 1000.0) / 1000.0;
+        let b = Math.round(imageData[2] / 255.0 * 1000.0) / 1000.0;
+        colorText.value = "vec3(" + r + ', ' + g + ', ' + b + ")\nvec4(" + r + ', ' + g + ', ' + b + ", 1.0)";
+    }
+
+    function clickStrip(e) {
+        let x = e.offsetX;
+        let y = e.offsetY;
+        var imageData = ctx2.getImageData(x, y, 1, 1).data;
+        rgbaColor = 'rgba(' + imageData[0] + ',' + imageData[1] + ',' + imageData[2] + ',1)';
+        fillGradient();
+        changeColor();
+    }
+
+    colorStrip.addEventListener("mousedown", e => { 
+        dragStrip = true;
+        clickStrip(e);
+    }, false);
+
+    colorStrip.addEventListener("mouseup", () => { 
+        dragStrip = false 
+    }, false);
+
+    colorStrip.addEventListener("mousemove", e => {
+        if (dragStrip) {
+            clickStrip(e);
+        }
+    }, false);
+
+    colorBlock.addEventListener("mousedown", e => { 
+        drag = true;
+        changeColor(e);
+    }, false);
+
+    colorBlock.addEventListener("mouseup", () => { 
+        drag = false 
+    }, false);
+
+    colorBlock.addEventListener("mousemove", e => {
+        if (drag) {
+            changeColor(e);
+        }
+    }, false);
+
+    changeColor();
+}
+
+function updateDimensions(text, real) {
+    const dimensions = text.split(",").map(x => x.trim());
     const canvas = $("#glCanvas")[0];
-    canvas.width = dimensions[0];
-    canvas.height = dimensions[1];
-    canvas.style.width  = dimensions[0] + 'px';
-    canvas.style.height = dimensions[1] + 'px';
+    if(real) {
+        canvas.width = dimensions[0];
+        canvas.height = dimensions[1];
+    }
+    else {
+        canvas.style.width  = dimensions[0] + 'px';
+        canvas.style.height = dimensions[1] + 'px';
+    }
 }
 
 let texNo = 0;
 
+function addTexture(src, srcFilename) {
+    const $container = $("<div class='texture'></div>");
+
+    let texture;
+    let filename = "tex" + (texNo++);
+    
+    const $img = $("<img class='thumbnail'></img>");
+    $img.attr("src", src);
+    $container.append($img);
+
+    const $settings = $("<div class='texture-settings'></div>");
+    $settings.append("<br>");
+
+    const $props = $("<p class='texture-props'></p>");
+    $props.text(srcFilename);
+    $settings.append($props);
+
+    const $filename = $("<input type='text'>");
+    $filename.val(filename);
+    $filename.on("keypress", e => {
+        if(e.which == 13) {
+            textures[filename] = null;
+            filename = $filename.val();
+            textures[filename] = texture;
+            // reloadShader();
+            drawScene(gl, programInfo, squareBuffer);
+        }
+    });
+
+    $settings.append($filename);
+
+    const $delete = $("<button>X</button>");
+    $delete.click(() => {
+        textures[filename] = null;
+        $container.remove();
+        drawScene(gl, programInfo, squareBuffer);
+    });
+
+    $settings.append($delete);
+
+    loadTexture(gl, src, t => {
+        texture = t;
+        textures[filename] = texture;
+        const img = $img[0];
+        $props.html(srcFilename + "<br>size: " + img.naturalWidth + "," + img.naturalHeight);
+        // reloadShader();
+        
+        $container.append($settings);
+        
+        $("#texture-list").append($container);
+
+        drawScene(gl, programInfo, squareBuffer);
+    }, () => {
+        alert("Could not load image.");
+    });
+}
+
+function getFilename(path) {
+    path = path.replace(/\\/gi, "/");
+    return path.substring(path.lastIndexOf("/") + 1);
+}
+
 window.onload = () => {
-    main();
 
     $("#file-upload").on("change", function() {
         if (this.files && this.files[0]) {
-            const src = URL.createObjectURL(this.files[0]); // set src to blob url
-            const $container = $("<div class='texture'></div>");
-
-            mainTex = loadTexture(gl, src, () => {
-                reloadShader();
-                drawScene(gl, programInfo, squareBuffer);
-            });
-
-            const $img = $("<img class='thumbnail'></img>");
-            $img.attr("src", src);
-            $container.append($img);
-
-            const $settings = $("<div class='texture-settings'></div>");
-            $settings.append("<br>");
-
-            const $size = $("<p>WxH</p>");
-            $settings.append($size);
-
-            const $filename = $("<input type='text'>");
-            $filename.val("tex" + (texNo++));
-            $settings.append($filename);
-
-            const $delete = $("<button>X</button>");
-            $settings.append($delete);
-
-            $container.append($settings);
-            
-            $("#texture-list").append($container);
+            const src = URL.createObjectURL(this.files[0]);
+            addTexture(src, getFilename(this.value));
         }
-        // console.log("AAAAAA");
     });
+
+    $("#file-src-box").on("keypress", function(e) {
+        if(e.which == 13) {
+            addTexture(this.value, getFilename(this.value));
+        }
+    })
 
     $("#reload-btn").click(() => {
         reloadShader();
         drawScene(gl, programInfo, squareBuffer);
-        console.log($("#frag-shader-code").val());
+        // console.log($("#frag-shader-code").val());
     });
 
-    $("#update-img-properties").click(() => {
-        updateDimensions();
-        drawScene(gl, programInfo, squareBuffer);
+    $("#output-dimensions").on("keypress", function(e) {
+        if(e.which == 13) {
+            updateDimensions(this.value, true);
+            drawScene(gl, programInfo, squareBuffer);
+        }
     });
+
+    $("#view-dimensions").on("keypress", function(e) {
+        if(e.which == 13) {
+            updateDimensions(this.value, true);
+            drawScene(gl, programInfo, squareBuffer);
+        }
+    });
+
+    main();
+
 };
 
 
